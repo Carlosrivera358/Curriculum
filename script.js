e46366272090cfdf1fb0f6055acc80808f1ee13e
Carlosrivera358 <carlitosnomegusta@gmail.com>
2024-10-21 23:30:52 -0500

    Actualizar
    Added JavaScript file for interactivity in the Curriculum project

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Este código puede ser utilizado para agregar interactividad en el futuro
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("El currículum ha cargado correctamente.");
+    // Aquí puedes agregar más funcionalidad si lo deseas.
+});
